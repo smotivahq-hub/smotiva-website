57087c26a8ceabea1db797e14f0657c461454759
Smart Moses <censusokoi515@gmail.com>
2025-11-18 19:12:46 +0100

    DONE

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -62,7 +62,7 @@ export default function HeroSection() {
               className="text-4xl sm:text-6xl lg:text-6xl font-heading font-bold text-white mb-6 leading-tighter tracking-tight"
             >
               <span className='text-accent-teal'>Smotiva</span> - Godswill ### Smart Creativity <br className="hidden sm:inline" /> 
-              for Digital Generation
+              for Digital Generation ####
             </motion.h1>
             
             {/* Animated Subtext (Using UI text + SEO/Human tweak) */}
